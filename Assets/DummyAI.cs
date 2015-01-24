5e28450bc4883ee07cb49b06f5ac71deabdd6f8d
UncleMeat2 <hillabyn@mymacewan.ca>
2015-01-23 23:22:25 -0700

    Nicks first push

---
+++ Assets/DummyAI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DummyAI : MonoBehaviour {
+
+	public Transform[] _Waypoints;
+	public int _NumofWaypoints;
+	public float _ObjectiveGap;
+	public int _WaypointCounter;
+
+	// Use this for initialization
+	void Start () {
+		_WaypointCounter=0;
+		_Waypoints= new Transform[_NumofWaypoints] ;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance(transform.position,_Waypoints[_WaypointCounter].position)< _ObjectiveGap){
+
+		}
+	}
+}
