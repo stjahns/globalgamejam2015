c7be785b305466537f5325f9f9c1601ae41f4bb1
Steve Jahns <s.t.jahns@gmail.com>
2015-01-24 22:37:53 -0700

    Wanderer dialog

--- Assets/DummyAI.cs
+++ Assets/DummyAI.cs
@@ -2,79 +2,81 @@
 using System.Collections;
 
 public class DummyAI : TriggerBase {
-	public float myz;
-	public GameObject ugly;
-	public Transform[] _Waypoints;
-	public int _NumofWaypoints;
-	public float _ObjectiveGap;
-	public int _WaypointCounter;
-	public float _Speed;
-	public Vector3 _Target;
-	public float _Distance;
-	public GameObject Body;
-	public RaycastHit2D hit;
-	public float POVwidth = 30f;
+    public float myz;
+    public GameObject ugly;
+    public Transform[] _Waypoints;
+    public int _NumofWaypoints;
+    public float _ObjectiveGap;
+    public int _WaypointCounter;
+    public float _Speed;
+    public Vector3 _Target;
+    public float _Distance;
+    public GameObject Body;
+    public RaycastHit2D hit;
+    public float POVwidth = 30f;
 
-	// Use this for initialization
-	void Start () {
-		Body= GameObject.FindGameObjectWithTag("Body");
-		_WaypointCounter= 0;
-		stop ();
-		_Target= _Waypoints[0].position;
-		_Distance= Vector3.Distance(transform.position,_Target);
-	//	_Waypoints= new Transform[_NumofWaypoints] ;
-	}
-	void awake(){
+    // Use this for initialization
+    void Start () {
+        Body= GameObject.FindGameObjectWithTag("Body");
+        _WaypointCounter= 0;
+        stop ();
+        _Target= _Waypoints[0].position;
+        _Distance= Vector3.Distance(transform.position,_Target);
+        //	_Waypoints= new Transform[_NumofWaypoints] ;
+    }
+    void awake(){
 
-	}
-	void setz(float SOBADANDWRONG){
-		myz = SOBADANDWRONG;
-	}
-	bool testsight(){
-	
-		if (  Mathf.Abs(Vector3.Angle (transform.position - Body.transform.position, transform.right))< POVwidth && hit.collider.CompareTag ("Body")){
-		
-			return true;
-		}
+    }
+    void setz(float SOBADANDWRONG){
+        myz = SOBADANDWRONG;
+    }
+    bool testsight(){
 
-		return false;
-	}
+        if (  Mathf.Abs(Vector3.Angle (transform.position - Body.transform.position, transform.right))< POVwidth && hit.collider.CompareTag ("Body")){
 
+            return true;
+        }
 
-
-	void Update () {
-		hit = Physics2D.Linecast(transform.position,Body.transform.position, 1 << LayerMask.NameToLayer("Body"));
-		Debug.DrawLine(transform.position,Body.transform.position);
+        return false;
+    }
 
 
-		if (testsight ()){
-			Debug.Log ("GAMMMMEOVER");                           //THEYSAW THE BODY
-		}
+
+    void Update () {
+        hit = Physics2D.Linecast(transform.position,Body.transform.position, 1 << LayerMask.NameToLayer("Body"));
+        Debug.DrawLine(transform.position,Body.transform.position);
 
 
-		_Distance=Vector3.Distance(transform.position,_Target);
-		if (_Distance< _ObjectiveGap){
-			if( _WaypointCounter == _Waypoints.Length - 1){
-				_WaypointCounter=0;
-			}
-			else{
-				_WaypointCounter++;
-			}
-			_Target= _Waypoints[_WaypointCounter].position;
-		}
-		transform.position= Vector3.MoveTowards (transform.position,_Target,1f*_Speed* Time.deltaTime);
-		ugly.SendMessage("gimmie",_Target);
-		transform.eulerAngles= new Vector3 (0f,0f,myz);// transform.eulerAngles(0f,0f, myz);
-	
-	}
-	[InputSocket]
-	public void stop(){
-		_Speed=0f;
-	}
-	[InputSocket]
-	public void go(){
-		_Speed=5f;
-	}
+        if (testsight ()){
+            Debug.Log ("GAMMMMEOVER");                           //THEYSAW THE BODY
+        }
+
+
+        _Distance=Vector3.Distance(transform.position,_Target);
+        if (_Distance< _ObjectiveGap){
+            if( _WaypointCounter == _Waypoints.Length - 1){
+                _WaypointCounter=0;
+            }
+            else{
+                _WaypointCounter++;
+            }
+            _Target= _Waypoints[_WaypointCounter].position;
+        }
+        transform.position= Vector3.MoveTowards (transform.position,_Target,1f*_Speed* Time.deltaTime);
+        ugly.SendMessage("gimmie",_Target);
+        transform.eulerAngles= new Vector3 (0f,0f,myz);// transform.eulerAngles(0f,0f, myz);
+
+    }
+
+    [InputSocket]
+    public void stop(){
+        _Speed=0f;
+    }
+
+    [InputSocket]
+    public void go(){
+        _Speed=5f;
+    }
 
 
 
--- Assets/ThecoupleAI.cs
+++ Assets/ThecoupleAI.cs
@@ -3,108 +3,108 @@ using System.Collections;
 
 public class ThecoupleAI : MonoBehaviour {
 
-	public float myz;
-	public GameObject ugly;
-	public bool guy;
-	public int timer;
-	public int counter;
-	public GameObject Body;
-	public int _WaypointCounter;
-	public bool doit;
-	public bool undoit;
-	public Transform[] _Waypoints;
-	public float _ObjectiveGap;
-	public float _Speed;
-	public RaycastHit2D hit;
+  public float myz;
+  public GameObject ugly;
+  public bool guy;
+  public int timer;
+  public int counter;
+  public GameObject Body;
+  public int _WaypointCounter;
+  public bool doit;
+  public bool undoit;
+  public Transform[] _Waypoints;
+  public float _ObjectiveGap;
+  public float _Speed;
+  public RaycastHit2D hit;
 
-	public Vector3 _Target;
-	public float _Distance;
-	// Use this for initialization
-	void Start () {
-		 Body= GameObject.FindGameObjectWithTag("Body");
-		_Target= _Waypoints[1].position;
-	}
-	void Go(){
-		
-	}
-	void setz(float SOBADANDWRONG){
-		myz = SOBADANDWRONG;
-	}
-	// Update is called once per frame
+  public Vector3 _Target;
+  public float _Distance;
+  // Use this for initialization
+  void Start () {
+     Body= GameObject.FindGameObjectWithTag("Body");
+    _Target= _Waypoints[1].position;
+  }
+  void Go(){
 
-	void Update () {
-		hit = Physics2D.Raycast(new Vector2(transform.position.x,transform.position.y),transform.rigidbody2D.position - Body.rigidbody2D.position);
+  }
+  void setz(float SOBADANDWRONG){
+    myz = SOBADANDWRONG;
+  }
+  // Update is called once per frame
+
+  void Update () {
+    hit = Physics2D.Raycast(new Vector2(transform.position.x,transform.position.y),transform.rigidbody2D.position - Body.transform.position.XY());
 //		Debug.Log (hit.collider.name);
 //		Debug.DrawLine(transform.position,Body.transform.position);
 //		if (testsight()){}                  //THEY SAW THE BODY!!!!
 
-		if (!doit){
-			counter++;
-		}
-		if (counter >= timer && !undoit){
-			doit = true;                            //THIS SHOULD REALLY NOT RUN ON A TRIGGER AND SHOULD GET CALLED FROM ELSEWHERE
-		}
-		if(doit){
-			_Distance= Vector3.Distance(transform.position,_Target);
-			if (_Distance < _ObjectiveGap){
-				if( _WaypointCounter < _Waypoints.Length - 1){
-					_WaypointCounter++;
-					_Target= _Waypoints[_WaypointCounter].position;
-					//doit = false;
-					//_WaypointCounter--;
-				}
-				else{
-					Debug.Log ("HEY CODE ME MUTHSFUCKER");   // do something on a coroutine probably
-															//HOwever, let.s back em up
-					doit= false;
-					counter = 0;
-					undoit = true;
-					_WaypointCounter--;
-					_Target= _Target= _Waypoints[_WaypointCounter].position;
-					_Distance= Vector3.Distance(transform.position,_Target);
-				}
+    if (!doit){
+      counter++;
+    }
+    if (counter >= timer && !undoit){
+      doit = true;                            //THIS SHOULD REALLY NOT RUN ON A TRIGGER AND SHOULD GET CALLED FROM ELSEWHERE
+    }
+    if(doit){
+      _Distance= Vector3.Distance(transform.position,_Target);
+      if (_Distance < _ObjectiveGap){
+        if( _WaypointCounter < _Waypoints.Length - 1){
+          _WaypointCounter++;
+          _Target= _Waypoints[_WaypointCounter].position;
+          //doit = false;
+          //_WaypointCounter--;
+        }
+        else{
+          Debug.Log ("HEY CODE ME MUTHSFUCKER");   // do something on a coroutine probably
+                              //HOwever, let.s back em up
+          doit= false;
+          counter = 0;
+          undoit = true;
+          _WaypointCounter--;
+          _Target= _Target= _Waypoints[_WaypointCounter].position;
+          _Distance= Vector3.Distance(transform.position,_Target);
+        }
 
-			}
-			transform.position= Vector3.MoveTowards (transform.position,_Target,1f*_Speed* Time.deltaTime);
-			ugly.SendMessage("gimmie",_Target);
-			transform.eulerAngles= new Vector3 (0f,0f,myz);// transform.eulerAngles(0f,0f, myz);
-		}
-		if (undoit){
-			counter++;
-			if (counter > timer){
+      }
+      transform.position= Vector3.MoveTowards (transform.position,_Target,1f*_Speed* Time.deltaTime);
+      ugly.SendMessage("gimmie",_Target);
+      transform.eulerAngles= new Vector3 (0f,0f,myz);// transform.eulerAngles(0f,0f, myz);
+    }
+    if (undoit){
+      counter++;
+      if (counter > timer){
 
-				transform.position= Vector3.MoveTowards (transform.position,_Target,1f*_Speed* Time.deltaTime);
-				ugly.SendMessage("gimmie",_Target);
-				transform.eulerAngles= new Vector3 (0f,0f,myz);// transform.eulerAngles(0f,0f, myz);
-				_Distance= Vector3.Distance(transform.position,_Target);
-				if (_Distance < _ObjectiveGap){
-					if( _WaypointCounter > 0) {
-						_WaypointCounter--;
-						_Target= _Waypoints[_WaypointCounter].position;
-				}
-				else {
-					undoit = false;
-						counter = 0;
-					_WaypointCounter= 0;
+        transform.position= Vector3.MoveTowards (transform.position,_Target,1f*_Speed* Time.deltaTime);
+        ugly.SendMessage("gimmie",_Target);
+        transform.eulerAngles= new Vector3 (0f,0f,myz);// transform.eulerAngles(0f,0f, myz);
+        _Distance= Vector3.Distance(transform.position,_Target);
+        if (_Distance < _ObjectiveGap){
+          if( _WaypointCounter > 0) {
+            _WaypointCounter--;
+            _Target= _Waypoints[_WaypointCounter].position;
+        }
+        else {
+          undoit = false;
+            counter = 0;
+          _WaypointCounter= 0;
 
-					}
-				}
-			}
-		}
+          }
+        }
+      }
+    }
 
-	/*	_Distance=Vector3.Distance(transform.position,_Target);
-		if (_Distance< _ObjectiveGap){
-			if( _WaypointCounter == _Waypoints.Length - 1){
-				_WaypointCounter=0;
-			}
-			else{
-				_WaypointCounter++;
-			}
-			_Target= _Waypoints[_WaypointCounter].position;
-		}
-		transform.position= Vector3.MoveTowards (transform.position,_Target,1f*_Speed* Time.deltaTime);
-		transform.position= Vector3.MoveTowards (transform.position,_Target,1f*_Speed* Time.deltaTime);
-	}*/
-	
-	}
+  /*	_Distance=Vector3.Distance(transform.position,_Target);
+    if (_Distance< _ObjectiveGap){
+      if( _WaypointCounter == _Waypoints.Length - 1){
+        _WaypointCounter=0;
+      }
+      else{
+        _WaypointCounter++;
+      }
+      _Target= _Waypoints[_WaypointCounter].position;
+    }
+    transform.position= Vector3.MoveTowards (transform.position,_Target,1f*_Speed* Time.deltaTime);
+    transform.position= Vector3.MoveTowards (transform.position,_Target,1f*_Speed* Time.deltaTime);
+  }*/
+
+  }
 }
