c7441ebe8f0b28001708b7f2ee2bf68c3b3651d6
Steve Jahns <s.t.jahns@gmail.com>
2015-01-25 07:24:19 -0700

    Final scene

---
+++ Assets/EndScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndScene : TriggerBase {
+
+
+    public UnityEngine.UI.Image image;
+
+    [InputSocket]
+    public void Show()
+    {
+        image.enabled = true;
+    }
+
+}
